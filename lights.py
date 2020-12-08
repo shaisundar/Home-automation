bd547ee86cd79acae98d8e4d7d5c99dc44d7d079
shaisundar <30902543+shaisundar@users.noreply.github.com>
2020-12-08 14:32:50 +0530

    Update lights.py

--- lights.py
+++ lights.py
@@ -13,7 +13,7 @@ GPIO.output(31, GPIO.HIGH)
 # Set to your Adafruit IO key.
 # Remember, your key is a secret,
 # so make sure not to publish it when you publish this code!
-ADAFRUIT_IO_KEY = 'aio_TthI33he8iu1PHPmxSjKqC1l5STK'
+ADAFRUIT_IO_KEY = 'key-here'
 
 # Set to your Adafruit IO username.
 # (go to https://accounts.adafruit.com to find your username)
